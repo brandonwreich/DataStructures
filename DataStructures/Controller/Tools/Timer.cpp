84758034c7a4dd23a468a0e9543fa1c673654124
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2018-01-31 07:50:21 -0700

    Created the timer class and added a constructor

---
+++ DataStructures/Controller/Tools/Timer.cpp
@@ -0,0 +1,9 @@
+//
+//  Timer.cpp
+//  DataStructures
+//
+//  Created by Reich, Brandon on 1/31/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "Timer.hpp"
---
+++ DataStructures/Controller/Tools/Timer.hpp
@@ -0,0 +1,21 @@
+//
+//  Timer.hpp
+//  DataStructures
+//
+//  Created by Reich, Brandon on 1/31/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Timer_hpp
+#define Timer_hpp
+
+#include <time.h>
+#include <assert.h>
+#include <iostream>
+
+class Timer
+{
+    
+}
+
+#endif /* Timer_hpp */
