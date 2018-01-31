3f6441677aa6b56c530fcfa031bb35886a552b2f
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2018-01-31 07:46:28 -0700

    Added a new class and started to develop it

---
+++ DataStructures/Controller/FileController.hpp
@@ -0,0 +1,18 @@
+//
+//  FileController.hpp
+//  DataStructures
+//
+//  Created by Reich, Brandon on 1/31/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef FileController_hpp
+#define FileController_hpp
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+
+#endif /* FileController_hpp */
