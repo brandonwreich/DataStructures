4cb6994db2aef2b8797cb76607d3d11691402073
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2018-01-31 07:46:09 -0700

    Created my project and set up the basic runner and controller

---
+++ DataStructures/Controller/Controller.hpp
@@ -0,0 +1,20 @@
+//
+//  Controller.hpp
+//  DataStructures
+//
+//  Created by Reich, Brandon on 1/31/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+#include "FileController.hpp"
+#include <vector>
+
+class Controller
+{
+public:
+    void start();
+};
+#endif /* Controller_hpp */
