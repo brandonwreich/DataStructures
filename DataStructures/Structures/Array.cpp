c57ac204c510f577cb12be04284c5ccdf6723548
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2018-02-02 09:02:14 -0700

    Implementing methods

--- DataStructures/Structures/Array.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Array.cpp
-//  DataStructures
-//
-//  Created by Reich, Brandon on 2/2/18.
-//  Copyright © 2018 CTEC. All rights reserved.
-//
-
-#include "Array.hpp"
--- DataStructures/Structures/Array.hpp
+++ DataStructures/Structures/Array.hpp
@@ -17,7 +17,84 @@ using namespace std;
 template <class Type>
 class Array
 {
+private:
+    Type * internalArray;
+    int size;
+public:
+    // Contructor
+    Array<Type>(int size);
     
+    // Copy constructor
+    Array<Type>(const Array<Type> & toCopy);
+    
+    // Destructor
+    ~Array<Type>();
+    
+    // Operators
+    Array<Type> & operator = (const Array<Type> & toReplace);
+    Type& operator [] (int index);
+    Type operator [] (int index) const;
+    
+    // Methods
+    int getSize() const;
+    Type getFromIndex(int index);
+    void setAtIndex(int index, Type data);
+};
+
+template <class Type>
+Array<Type> :: Array(int size)
+{
+    assert(size > 0);
+    this -> size = size;
+    
+    internalArray = new Type[size];
+}
+
+template <class Type>
+Array<Type> :: Array(const Array<Type> & toCopy)
+{
+    this -> size = toCopy.getSize();
+    
+    // Build data structure
+    internalArray = new Type[size];
+    
+    for (int index = 0; index < size; index++)
+    {
+        internalArray[index] = toCopy[index];
+    }
+}
+
+template <class Type>
+Array<Type> :: ~Array()
+{
+    delete [] internalArray;
+}
+
+template <class Type>
+Array<Type> & Array<Type> :: operator = (const Array<Type> & toAssign)
+{
+    if (&toAssign!= toAssign.getSize())
+    {
+        if (size != toAssign.getSize())
+        {
+            delete [] internalArray;
+            size = toAssign.getSize();
+            internalArray = new Type [size];
+        }
+        
+        for (int index = 0; index < size; index++)
+        {
+            internalArray[index] = toAssign[index];
+        }
+    }
+    return * this;
+}
+
+template <class Type>
+Type & Array<Type> :: operator [] (int index)
+{
+    assert(index >= 0 && index < size);
+    return internalArray[index];
 }
 
 #endif /* Array_hpp */
