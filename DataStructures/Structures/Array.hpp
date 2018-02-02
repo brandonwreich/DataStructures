1eb3d606df26262994ada657ddf5b1beae4d2e85
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2018-02-02 08:44:51 -0700

    Created a new class and added the basics

---
+++ DataStructures/Structures/Array.hpp
@@ -0,0 +1,23 @@
+//
+//  Array.hpp
+//  DataStructures
+//
+//  Created by Reich, Brandon on 2/2/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Array_hpp
+#define Array_hpp
+
+#include <assert.h>
+#include <iostream>
+
+using namespace std;
+
+template <class Type>
+class Array
+{
+    
+}
+
+#endif /* Array_hpp */
