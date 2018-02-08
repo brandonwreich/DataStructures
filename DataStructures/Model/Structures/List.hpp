404eb5b8aab5691febcc3b587f83ef7dbe0b4d09
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2018-02-08 09:35:03 -0700

    Added the list class and implemented the methods

---
+++ DataStructures/Model/Structures/List.hpp
@@ -0,0 +1,33 @@
+//
+//  List.hpp
+//  DataStructures
+//
+//  Created by Reich, Brandon on 2/8/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef List_hpp
+#define List_hpp
+
+#include <assert.h>
+#include "../LinearNode.hpp"
+
+template class <Type>
+class List
+{
+protected:
+    int size;
+public:
+    // Structure
+    virtual void add(Type item) = 0;
+    virtual void addAtIndex(int index, Type item) = 0;
+    virtual Type remove(Type item) = 0;
+    virtual Type getFromIndex(int index) = 0;
+    
+    // Helper
+    virtual int getSize() const = 0;
+    virtual LinearNode<Type> * getFront() = 0;
+    virtual LinearNode<Type> * getEnd() = 0;
+};
+
+#endif /* List_hpp */
