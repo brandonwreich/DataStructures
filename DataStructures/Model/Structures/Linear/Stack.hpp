46e9243177e2db805069e2de2f180f7e0e7a8b3b
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2018-02-22 08:20:58 -0700

    created the Stack class

---
+++ DataStructures/Model/Structures/Linear/Stack.hpp
@@ -0,0 +1,19 @@
+//
+//  Stack.hpp
+//  DataStructures
+//
+//  Created by Reich, Brandon on 2/22/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Stack_hpp
+#define Stack_hpp
+
+#include "LinkedList.hpp"
+
+template <class Type>
+class Stack : public LinkedList<Type>
+{
+}
+
+#endif /* Stack_hpp */
