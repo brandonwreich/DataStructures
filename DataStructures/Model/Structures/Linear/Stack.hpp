2a31bd7cba3f009f816edc99a9e16b5251e71859
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2018-02-22 08:22:54 -0700

    Made the push method which adds data to the stack and increases the size

--- DataStructures/Model/Structures/Linear/Stack.hpp
+++ DataStructures/Model/Structures/Linear/Stack.hpp
@@ -45,6 +45,23 @@ Stack<Type> :: ~Stack()
     }
 }
 
+template <class Type>
+void Stack<Type> :: push(Type data)
+{
+    LinearNode<Type> * add = new LinearNode<Type>(data);
+    
+    if (this -> size == 0)
+    {
+        this -> end = add;
+    }
+    else
+    {
+        add -> setNextNode(this -> front);
+    }
+    
+    this -> front = add;
+    this -> size++;
+}
 }
 
 #endif /* Stack_hpp */
