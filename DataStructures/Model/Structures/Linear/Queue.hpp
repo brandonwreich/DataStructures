4551c41661fe150ab2ed554af9c08072abc001cc
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2018-02-22 08:38:44 -0700

    Wrote a method to add the Queue. Addes linear nodes only at the end of the queue

--- DataStructures/Model/Structures/Linear/Queue.hpp
+++ DataStructures/Model/Structures/Linear/Queue.hpp
@@ -50,4 +50,22 @@ Queue<Type> :: ~Queue()
     }
 }
 
+template <class Type>
+void Queue<Type> :: enqueue(Type item)
+{
+    LinearNode<Type> * added = new LinearNode<Type>(item);
+    
+    if (this -> size == 0)
+    {
+        this -> front = added;
+    }
+    else
+    {
+        this -> end -> setNextNode(added);
+    }
+    
+    this -> end = added;
+    this -> size++;
+}
+
 #endif /* Queue_hpp */
