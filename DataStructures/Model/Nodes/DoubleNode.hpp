0dce3eb466cdfc412909dd783a0b15fbfe1f84c8
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2018-02-26 07:59:42 -0700

    Declared all of my methods

--- DataStructures/Model/Nodes/DoubleNode.hpp
+++ DataStructures/Model/Nodes/DoubleNode.hpp
@@ -10,4 +10,23 @@
 
 #ifndef DoubleNode_hpp
 #define DoubleNode_hpp
+
+template <class Type>
+class DoubleNode : public Node<Type>
+{
+protected:
+    DoubleNode<Type> * previous;
+    DoubleNode<Type> * next;
+public:
+    DoubleNode();
+    DoubleNode(Type data);
+    DoubleNode(Type data, DoubleNode<Type> * previous, DoubleNode<Type> * next);
+    
+    DoubleNode<Type> * getPrevious();
+    DoubleNode<Type> * getNext();
+    
+    void setPrevious(DoubleNode<Type> * previous);
+    void setNext(DoubleNode<Type> * next);
+};
+
 #endif /* DoubleNode_hpp */
