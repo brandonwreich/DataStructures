c63fdece077861ad8df873a0df0cd9cc2004e7db
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2018-02-26 07:58:56 -0700

    Created the DoubleNode.hpp file

---
+++ DataStructures/Model/Nodes/DoubleNode.hpp
@@ -0,0 +1,13 @@
+//
+//  DoubleNode.hpp
+//  DataStructures
+//
+//  Created by Reich, Brandon on 2/26/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "Node.hpp"
+
+#ifndef DoubleNode_hpp
+#define DoubleNode_hpp
+#endif /* DoubleNode_hpp */
