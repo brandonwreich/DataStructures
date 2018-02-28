79bbcb71461fdd43069855b469eb6ef3a3be983f
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2018-02-28 08:28:31 -0700

    Minor changes and added documentation

--- DataStructures/Testers/ArrayTester.cpp
+++ DataStructures/Testers/ArrayTester.cpp
@@ -10,44 +10,56 @@
 
 void ArrayTester :: testArrayUse()
 {
+    // Initalize array
     Array<int> firstArray(10);
     
+    // Fill the array
     for (int index = 0; index < 10; index++)
     {
         firstArray.setAtIndex(index, index);
     }
     
-    for (int index= 0; index < firstArray.getSize(); index++)
+    // Print the array
+    for (int index = 0; index < firstArray.getSize(); index++)
     {
         cout << firstArray[index] << endl;
     }
     
+    // Initalize 2nd array
     Array<int> secondArray(400);
     
+    // Print the sizes of the arrays
     cout << secondArray.getSize() << endl;
     cout << firstArray.getSize() << endl;
     
+    // Intialize 3rd array
     Array<int> thirdArray(secondArray);
     
+    // Print  3rd array size
     cout << thirdArray.getSize() << endl;
+    
     firstArray = thirdArray;
+    
+    // Prints the 4th index of the 1st array
     cout << firstArray[4] << endl;
     
+    // Adds 123 to the 5th index in the 3rd array and prints it
     thirdArray[5] = 123;
     cout << thirdArray[5] << endl;
-    
-    
 }
 
 void ArrayTester :: testAdvancedArray()
 {
+    // Initalize data members
     Array<int> array(912);
     
+    // Add numbers to the list
     for (int index = 0; index < 912; index++)
     {
         array.setAtIndex(index, index);
     }
     
+    // Print the list
     for (int index = 0; index < array.getSize(); index++)
     {
         cout << array[index] << '\t';
--- DataStructures/Testers/LinkedListTester.cpp
+++ DataStructures/Testers/LinkedListTester.cpp
@@ -10,55 +10,87 @@
 
 void LinkedListTester :: testListBasics()
 {
+    // Initalize data members
     LinkedList<int> numbers;
     
+    // Add 9 to the end of the list
     numbers.add(9);
+    
+    // Prints the front and end
     cout << numbers.getFront() -> getData() << " is at the front of the list and should be 9" << endl;
     cout << numbers.getEnd() -> getData() << " is at the end of the list and should be 9" << endl;
     
+    // Add 0 to the end of the list
     numbers.add(0);
+    
+    // Prints front and end
     cout << numbers.getFront() -> getData() << " is at the front of the list and should be 9" << endl;
     cout << numbers.getEnd() -> getData() << " is at the end of the list and should be 0" << endl;
     
+    // Add 2 at index 1 and 324 to the end of the list
     numbers.addAtIndex(1,2);
     numbers.add(324);
+    
+    // Prints the front and end
     cout << numbers.getFront() -> getData() << " is at the front of the list and should be 9" << endl;
     cout << numbers.getEnd() -> getData() << " is at the end of the list and should be 324" << endl;
     
     cout << "This loop should print 4 lines" << endl;
+    
+    // Loop to print the list
     for (int  index = 0; index < numbers.getSize(); index++)
     {
         cout << numbers.getFromIndex(index) << endl;
     }
     
+    // Removes number at index 0
     numbers.remove(0);
+    
+    // Addes 32567 to the end of the list and 2312 at the front of the list
     numbers.add(32567);
     numbers.addAtIndex(0, 2312);
     
+    // Prints the front and end
     cout << numbers.getFront() -> getData() << " is at the front of the list and should be 2312" << endl;
     cout << numbers.getEnd() -> getData() << " is at the end of the list and shold be 32567" << endl;
 }
 
 void LinkedListTester :: testListWithData()
 {
+    // Iniatlize data members
     Timer listTimer;
     
+    // Start timer
     listTimer.startTimer();
+    
+    // Read the crime stats
     LinkedList<CrimeData> crimes = FileController :: readDataToList("/Users/brei8876/Documents/Data Structures/DataStructures/DataStructures/Data/crime.csv");
+    
+    // Stop timer
     listTimer.stopTimer();
     
+    // Display information
     cout << "This is houw long it took to read the structure into our custom data structure" << endl;
     listTimer.displayInformation();
     
+    // Reset timer
     listTimer.resetTimer();
     cout << "Here is how long it takes to access a random data value" << endl;
     
+    // Start timer
     listTimer.startTimer();
+    
+    // Initalize data members
     int randomLocation = (rand() * rand()) % crimes.getSize();
+    
+    // Grab a random stat
     cout << "The random index is " << randomLocation << endl;
     double totalViolentRate = crimes.getFromIndex(randomLocation).getAllViolentRates();
+    
+    // Stop timer
     listTimer.stopTimer();
     
+    // Display information
     cout << "The random crime stat is: " << totalViolentRate << " and here is the time" << endl;
     listTimer.displayInformation();
     
