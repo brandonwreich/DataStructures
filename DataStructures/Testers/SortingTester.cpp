f34c53c034cb3f75d061b2e88a1b6644b60f96c0
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2018-03-15 08:27:43 -0600

    Added a small change to the code

--- DataStructures/Testers/SortingTester.cpp
+++ DataStructures/Testers/SortingTester.cpp
@@ -42,15 +42,19 @@ void SortingTester :: swap(vector<CrimeData> info, int small, int large)
     CrimeData temp = info[small];
     info[small] = info[large];
     info[large] = temp;
+    swapCount++;
 }
 
 void SortingTester :: testSorts()
 {
     Timer sortTimer;
+    swapCount = 0;
     vector<CrimeData> data = FileController :: readCrimeDataToVector("/Users/brei8876/Documents/Data Structures/DataStructures/DataStructures/Data/crime.csv");
     
     sortTimer.startTimer();
     quickSort(data, 0, data.size());
     sortTimer.stopTimer();
     sortTimer.displayInformation();
+    
+    cout << "The number of swaps were: " << swapCount << endl;
 }
