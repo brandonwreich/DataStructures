23fe5ba0d62a899ee591825854927d8fa9d49784
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2018-03-29 08:08:32 -0600

    Added the  BinarySearchTree class and inserted all of my protected methods

---
+++ DataStructures/Model/Structures/NonLinear/BinarySearchTree.hpp
@@ -0,0 +1,38 @@
+//
+//  BinarySearchTree.hpp
+//  DataStructures
+//
+//  Created by Reich, Brandon on 3/29/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef BinarySearchTree_hpp
+#define BinarySearchTree_hpp
+
+#include <stdio.h>
+
+using namespace std;
+
+template <class Type>
+class BinarySearchTree : public Tree<Type>
+{
+protected:
+    int calculateSize(BinaryTreeNode<Type> * startNode);
+    int calculateHeight(BinaryTreeNode<Type> * startNode);
+    bool isBalanced(BinaryTreeNode<Type> * startNode);
+    bool isComplete(BinaryTreeNode<Type> * startNode, int index, int size);
+    
+    void inOrderTraversal(BinaryTreeNode<Type> * intStart);
+    void preOrderTraversal(BinaryTreeNode<Type> * preStart);
+    void postOrderTraversal(BinaryTreeNode<Type> * postStart);
+    
+    BinaryTreeNode<Type> * getRightMostChild(BinaryTreeNode<Type> * current);
+    BinaryTreeNode<Type> * getRightMostChild(BinaryTreeNode<Type> * current);
+    
+    void removeNode(BinaryTreeNode<Type> * removeMe);
+public:
+    BinarySearchTree();
+    ~BinarySearchTree();
+}
+
+#endif /* BinarySearchTree_hpp */
