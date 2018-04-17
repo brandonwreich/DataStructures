2d0245cd229f0fffe6ef3294a4b1407adcbd3bed
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2018-04-17 09:03:41 -0600

    wrote the rotation methods

--- DataStructures/Model/Structures/NonLinear/AVLTree.hpp
+++ DataStructures/Model/Structures/NonLinear/AVLTree.hpp
@@ -48,4 +48,83 @@ int AVLTree<Type> :: heightDifference(BinaryTreeNode<Type> * node)
     return balance;
 }
 
+template <class Type>
+BinaryTreeNode<Type> * AVLTree<Type> :: leftRotation(BinaryTreeNode<Type> * parent)
+{
+    BinaryTreeNode<Type> * changedNode;
+    changedNOde = parent -> getLeftNode();
+    
+    parent -> setLeftChild(changedNode -> getRightNode());
+    
+    changedNode -> setRightChild(parent);
+    
+    return changedNode;
+}
+
+template <class Type>
+BinaryTreeNode<Type> * AVLTree<Type> :: rightRotation(BinaryTreeNode<Type> * parent)
+{
+    BinaryTreeNode<Type> * changedNode;
+    changeNode = parent -> getRightNode();
+    
+    parent -> setRightChild(changedNode -> getLeftNode());
+    
+    changedNode -> setLeftChild(parent);
+    
+    return changedNode;
+}
+
+template <class Type>
+BinaryTreeNode<Type> * AVLTree<Type> :: rightLeftRotation(BinaryTreeNode<Type> * parent)
+{
+    BinaryTreeNode<Type> * changedNode;
+    changedNode = parent -> getRightNode();
+    
+    parent -> setRightChild(leftRotation(changedNode));
+    
+    return rightRotation(parent);
+}
+
+template <class Type>
+BinaryTreeNode<Type> * AVLTree<Type> :: leftRightRotation(BinaryTreeNode<Type> * parent)
+{
+    BinaryTreeNode<Type> * changedNode;
+    changedNode = parent -> getLeftNode();
+    
+    parent -> setLeftChild(rightRotation(changedNode));
+    
+    return leftRotation(parent);
+}
+
+template <class Type>
+BinaryTreeNode<Type> * AVLTree<Type> :: balanceSubTree(BinaryTreeNode<Type> * parent)
+{
+    int balanceFactor = heightDifference(parent);
+    
+    if(balanceFactor > 1)
+    {
+        if(heightDifference(parent -> getLeftNode()) > 0)
+        {
+            parent = leftRotation(parent);
+        }
+        else
+        {
+            parent = leftRightRotation(parent);
+        }
+    }
+    else if (balanceFactor < -1)
+    {
+        if(heightDifference(parent -> getRightNode()) > 0)
+        {
+            parent = rightLeftRotation(parent);
+        }
+        else
+        {
+            parent = rightRotation(parent);
+        }
+    }
+    
+    return parent;
+}
+
 #endif /* AVLTree_hpp */
