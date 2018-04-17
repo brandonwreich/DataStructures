85fe3e103f75a79e6cd6ddbfda9a45a9ade59ef1
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2018-04-17 09:53:34 -0600

    Finished the class

--- DataStructures/Model/Structures/NonLinear/AVLTree.hpp
+++ DataStructures/Model/Structures/NonLinear/AVLTree.hpp
@@ -127,4 +127,85 @@ BinaryTreeNode<Type> * AVLTree<Type> :: balanceSubTree(BinaryTreeNode<Type> * pa
     return parent;
 }
 
+template <class Type>
+BinaryTreeNode<Type> * AVLTree<Type> :: removeNode(BinaryTreeNode<Type> * parent, Type inserted)
+{
+    if(parent == nullptr)
+    {
+        return parent;
+    }
+    if(inserted < parent -> getNodeData())
+    {
+        parent -> setLeftChild(removeNode(parent -> getLeftNode(), inserted));
+    }
+    else if(inserted > parent -> getNodeData())
+    {
+        parent -> setRightChild(removeNode(parent -> getRightNode(), inserted));
+    }
+    else
+    {
+        BinaryTreeNode<Type> * temp;
+        if(parent -> getLeftNode() == nullptr && parent -> getRightNode() == nullptr)
+        {
+            temp = parent;
+            delete temp;
+        }
+        else if(parent -> getLeftNode() == nullptr)
+        {
+            *parent = *parent -> getRightNode();
+        }
+        else if(parent -> getRightNode() == nullptr)
+        {
+            *parent = *parent -> getLeftNode();
+        }
+        else
+        {
+            BinaryTreeNode<Type> * leftMost = this -> getLeftMostChild(parent -> getRightNode());
+            parent -> setNodeData(leftMost -> getData());
+            parent -> setRightChild(removeNode(parent -> getRightNode(), leftMost -> getNodeData()));
+        }
+    }
+    
+    if(parent == nullptr)
+    {
+        return parent;
+    }
+    
+    return balanceSubTree(parent);
+}
+
+template <class Type>
+BinaryTreeNode<Type> * AVLTree<Type> :: insertNode(BinaryTree<Type> * parent, Type inserted)
+{
+    if (parent == nullptr)
+    {
+        parent = new BinaryTreeNode<Type>(inserted);
+        return parent;
+    }
+    else if(inserted < parent -> getNodeData())
+    {
+        parent -> setLeftChild(insertedNode(parent -> getLeftNode(), inserted));
+        parent = balancedSubTree(parent);
+    }
+    else if(inserted > parent -> getNodeData())
+    {
+        parent -> setRightChild(insertNode(parent -> getRightNode(), inserted));
+        parent = balanceSubTree(parent);
+    }
+    
+    return parent;
+}
+
+template <class Type>
+void AVLTree<Type> :: insert(Type item)
+{
+    insetNode(this -> getRoot(), item);
+}
+
+template <class Type>
+void AVLTree<Type> :: remove(Type item)
+{
+    removeNode(this -> getRoot(), item);
+}
+
 #endif /* AVLTree_hpp */
