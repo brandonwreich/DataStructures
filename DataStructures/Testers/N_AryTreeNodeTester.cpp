dc28883cc2caf4919b88b1b38db010900190b4a8
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2018-04-23 08:53:48 -0600

    Added a testing class

---
+++ DataStructures/Testers/N_AryTreeNodeTester.cpp
@@ -0,0 +1,9 @@
+//
+//  N_AryTreeNodeTester.cpp
+//  DataStructures
+//
+//  Created by Reich, Brandon on 4/23/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "N_AryTreeNodeTester.hpp"
---
+++ DataStructures/Testers/N_AryTreeNodeTester.hpp
@@ -0,0 +1,15 @@
+//
+//  N_AryTreeNodeTester.hpp
+//  DataStructures
+//
+//  Created by Reich, Brandon on 4/23/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef N_AryTreeNodeTester_hpp
+#define N_AryTreeNodeTester_hpp
+
+#include <iostream>
+#include "../Model/Nodes/N_AryTreeNode.hpp"
+
+#endif /* N_AryTreeNodeTester_hpp */
